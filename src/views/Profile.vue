0b16aa3deadd36b140ca5422f771e68d369203b3
Richard Trujillo <richard.trujillo.torres@gmail.com>
2022-06-21 00:00:20 +0200

    Update profile view

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,13 +1,65 @@
 <template>
   <div>
-    <h1>Profile</h1>
+    <div class="card">
+      <div class="card-title">
+        <h1>Profile</h1>
+      </div>
+      <div class="card-body" v-if="this.user">
+        <form @submit.prevent="onUpdate">
+          <div class="form-group">
+            <label for="exampleInputEmail1">Email</label>
+            <input disabled type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" :placeholder="user.email">
+            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+          </div>
+          <div class="form-group">
+            <label for="exampleInputPassword1">Name</label>
+            <input v-model="form.name" type="text" class="form-control" id="exampleInputPassword1" :placeholder="user.name">
+          </div>
+
+          <button type="submit" class="btn btn-primary">Update</button>
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import {mapActions, mapGetters} from "vuex";
+
 export default {
   name: 'Profile',
   components: {
+  },
+  data: () => ({
+    form: {
+      name: null,
+      email: null,
+    },
+  }),
+  computed: {
+    ...mapGetters({
+      'user': 'userModule/getUser',
+    })
+  },
+  methods: {
+    ...mapActions({
+      'fetchUser': 'userModule/fetchUser',
+      'setUser': 'userModule/setUser',
+    }),
+    onUpdate() {
+      this.axios.put(`/users`, {
+        name: this.form.name,
+      })
+      .then(res => {
+        this.setUser(res.data.data.user)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    },
+  },
+  beforeRouteEnter(to, from, next) {
+    next(vm => vm.fetchUser())
   }
 }
 </script>
