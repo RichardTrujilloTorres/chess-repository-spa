40133bab977e19a446eecebca088a17910727a87
Richard Trujillo <richard.trujillo.torres@gmail.com>
2022-06-24 02:31:49 +0200

    Add interceptors
    Emit "loading" event on request/response.

--- src/http/index.js
+++ src/http/index.js
@@ -1,9 +1,20 @@
 import axios from "axios";
+import {LOADING} from "../common/events";
+import emitter from "../common/emitter";
 
 axios.defaults.baseURL = process.env.VUE_APP_API_BASE_URL
 
 export default (app) => {
     app.axios = app.$http = axios
-
     app.config.globalProperties.axios = app.config.globalProperties.$http = axios;
+
+    app.axios.interceptors.request.use(config => {
+        emitter.emit(LOADING, true)
+        return config
+    }, err => Promise.reject(err))
+
+    app.axios.interceptors.response.use(res => {
+        emitter.emit(LOADING, false)
+        return res
+    }, err => Promise.reject(err))
 }
