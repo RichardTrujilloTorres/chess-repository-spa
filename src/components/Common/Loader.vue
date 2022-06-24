22b47200db4af556461d2972eacac408be8b4201
Richard Trujillo <richard.trujillo.torres@gmail.com>
2022-06-24 03:45:13 +0200

    Add loader component

---
+++ src/components/Common/Loader.vue
@@ -0,0 +1,37 @@
+<template>
+  <clip-loader :loading="loading" class="loader" :color="color" size="100px"></clip-loader>
+</template>
+
+<script>
+import ClipLoader from 'vue-spinner/src/ClipLoader.vue';
+import {mapGetters} from "vuex";
+
+export default {
+  name: 'Navbar',
+  components: {
+    ClipLoader,
+  },
+  computed: {
+    ...mapGetters({
+      loading: 'isLoading',
+    }),
+    color() {
+      return `#9A9A9A`
+    },
+  },
+  methods: {
+  },
+}
+</script>
+
+<style scoped>
+.loader {
+  left: 50%;
+  margin-left: -4em;
+  position: fixed;
+  top: 50%;
+  z-index: 9999999999;
+  overflow: hidden;
+  /*background: #fff;*/
+}
+</style>
--- src/components/registration.js
+++ src/components/registration.js
@@ -1,9 +1,11 @@
 import Navbar from "../components/Common/Navbar";
 import Search from "../components/Common/Search";
+import Loader from "./Common/Loader";
 
 const registration = app => {
     app.component('common_navbar', Navbar)
     app.component('common_search', Search)
+    app.component('common_loader', Loader)
 }
 
 export default registration
